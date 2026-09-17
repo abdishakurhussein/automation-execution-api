d980ecd0a32e0c3d7f08be3fc36b9f0c56245c7f
AbdishakurHussein <abdishakurhussein0@gmail.com>
2026-09-17 21:11:54 +0100

    Add Azure Container Apps continuous deployment

---
+++ terraform/github-actions.tf
@@ -0,0 +1,39 @@
+resource "azurerm_user_assigned_identity" "github_actions" {
+  name                = "id-github-automation-execution-dev"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
+resource "azurerm_federated_identity_credential" "github_master" {
+  name = "github-master"
+
+  # Attach this trust rule to GitHub's deployment identity.
+  user_assigned_identity_id = azurerm_user_assigned_identity.github_actions.id
+
+  audience = ["api://AzureADTokenExchange"]
+  issuer   = "https://token.actions.githubusercontent.com"
+
+  # Trust jobs from this repository's master branch.
+  subject = "repo:abdishakurhussein/automation-execution-api:ref:refs/heads/master"
+}
+
+resource "azurerm_role_assignment" "github_acr_push" {
+  # Limit registry permissions to this ACR.
+  scope                = data.azurerm_container_registry.existing.id
+  role_definition_name = "AcrPush"
+  principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
+}
+
+resource "azurerm_role_assignment" "github_container_app" {
+  # Allow deployment management only on this Container App.
+  scope                = azurerm_container_app.api.id
+  role_definition_name = "Container Apps Contributor"
+  principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
+}
+
+resource "azurerm_role_assignment" "github_runtime_identity" {
+  # Allow the deployment to use the app's existing runtime identity.
+  scope                = azurerm_user_assigned_identity.container_app.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,20 @@
 output "container_app_url" {
-  description = "Public HTTPS URL for the API"
-  value       = "https://${azurerm_container_app.api.latest_revision_fqdn}"
+  description = "Stable public HTTPS URL for the API"
+
+  # Use the application's address instead of a revision-specific address.
+  value = "https://${azurerm_container_app.api.ingress[0].fqdn}"
 }
 
 output "container_app_name" {
   value = azurerm_container_app.api.name
 }
+
+output "github_actions_client_id" {
+  description = "Client ID used by GitHub Actions to sign in to Azure"
+  value       = azurerm_user_assigned_identity.github_actions.client_id
+}
+
+output "github_actions_tenant_id" {
+  description = "Azure tenant containing the GitHub deployment identity"
+  value       = azurerm_user_assigned_identity.github_actions.tenant_id
+} 
